84104fa4cde74174b1f29021e27f623d4a73ee36
Andre Jarboe <andrejarboe@gmail.com>
2022-10-04 16:15:49 -0400

    make log function

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -8,9 +8,14 @@
 //preprocessor
 #include <iostream>
 
+void Log(const char* message) {
+	std::cout << message << std::endl;
+}
+
 int main() {
 	//std::cout.print("Hello World!!!").print(std::endl);
 	std::cout << "Hello World!!!" << std::endl;
+	Log("Hello from Log function");
 	//wait for enter
 	std::cin.get();
 
@@ -18,5 +23,9 @@ int main() {
 		header files get included into .cpp files
 		.cpp files get compiled individually into object files
 		the linker then glues all the .obj files into a .exe 
+		compile with ctrl f7 of compile button
+			no linking happens when you compile a single file
+			just mkaea an .opj file
+		build 
 	*/
 }
