581c10174ea679c08094f5192377f83310641713
Andre Jarboe <andrejarboe@gmail.com>
2022-10-10 12:40:10 -0400

    references

---
+++ HelloWorld/References.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
+
+//void Increment(int* value) {
+//	*value++;
+//}
+
+int main() {
+	//int a = 5;
+
+	//// make a ref
+	//// note & is apart of the type
+	//// this var doesn't actual exist
+	//// int& ref = a;
+
+	//Increment(&a);
+	//LOG(a);
+
+	LOG("hello");
+	std::cin.get();
+}
