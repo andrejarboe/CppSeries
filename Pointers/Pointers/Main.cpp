325f16bb60529df4a2a4690579c56e859d4c9aae
Andre Jarboe <andrejarboe@gmail.com>
2022-10-10 10:38:27 -0400

    dereferencing

--- Pointers/Pointers/Main.cpp
+++ Pointers/Pointers/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//#define LOG(x)
+#define LOG(x) std::cout << x << std::endl;
 
 int main() {
 
@@ -10,8 +10,16 @@ int main() {
 	* pointers help manage and manipulate memory
 	* memory is like one long street of houses
 	* each house has an address or one byte
-	* you need to read and write to the correct address or byte	*
+	* you need to read and write to the correct address or byte	
+	* a pointer does not need a type but it helps us know what should be there
 	*/
 
-	std::cout << "Hi" << std::endl;
+	int var = 8;
+	//assign memory address into the pointer
+	int* ptr = &var;
+	//dereferencing the pointer lets you write to the pointer
+	//now var = 10
+	*ptr = 10;
+	LOG(var);
+	std::cin.get();
 }
