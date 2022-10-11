e47cf4b46d08609859527b3bf088be89a62a21d1
Andre Jarboe <andrejarboe@gmail.com>
2022-10-10 21:22:06 -0400

    enums done

--- LearningCpp/LearningCpp/Logger.cpp
+++ LearningCpp/LearningCpp/Logger.cpp
@@ -4,27 +4,28 @@
 
 class ConsoleLog {
 public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
+	enum Level {
+		LevelError = 0, LevelWarning, LevelInfo 
+	};
+
 private:
-	int m_LogLevel = LogLevelInfo;
+	Level m_LogLevel = LevelInfo;
 
 public:
-	void SetLevel(int level) {
+	void SetLevel(Level level) {
 		m_LogLevel = level;
 	}
 
 	void Error(const char* message) {
-		if (m_LogLevel >= LogLevelError)
+		if (m_LogLevel >= LevelError)
 			std::cout << "[ERROR]: " << message << std::endl;
 	}
 	void Warn(const char* message) {
-		if (m_LogLevel >= LogLevelWarning)
+		if (m_LogLevel >= LevelWarning)
 			std::cout << "[WARNING]: " << message << std::endl;
 	}
 	void Info(const char* message) {
-		if (m_LogLevel >= LogLevelInfo)
+		if (m_LogLevel >= LevelInfo)
 			std::cout << "[INFO]: " << message << std::endl;
 	}
 };
@@ -35,8 +36,8 @@ void Logger() {
 	LOG("****************");
 
 	ConsoleLog consoleLog;
-	//consoleLog.SetLevel(consoleLog.LogLevelError);
-	consoleLog.Warn("Hello");
-	consoleLog.Error("Hello");
-	consoleLog.Info("Hello");
+	consoleLog.SetLevel(ConsoleLog::LevelError);
+	consoleLog.Warn("Hello!");
+	consoleLog.Error("Hello!");
+	consoleLog.Info("Hello!");
 }
