173e9a02aed17290cc9eeb73a7b47b2010f672c7
Andre Jarboe <andrejarboe@gmail.com>
2022-10-17 13:41:19 -0400

    LearnVirtualFunctions done

--- LearningCpp/LearningCpp/LearnVirtual Functions.cpp
+++ LearningCpp/LearningCpp/LearnVirtual Functions.cpp
@@ -4,9 +4,29 @@
 #include <string>
 #define LOG(x) std::cout << x << std::endl;
 
+
+/*
+* Virtual functions introduce
+something called dynamic dispatch
+which compilers typically implemented
+by our V table a V table is basically
+table which contains a mapping for all
+the virtual functions but our base class
+so that we can actually map them to the
+correct overwritten function at runtime
+
+if you want to override
+function you have to mark the base
+function in the base bar as virtual
+*/
 class Entity {
 public: 
-	std::string GetName() { return "Entity"; }
+	/*
+	* generate a V table for this function so
+	* that if it's overwritten you can point
+	* to the correct function with this change
+	*/
+	virtual std::string GetName() { return "Entity"; }
 };
 
 class Player : public Entity {
@@ -18,8 +38,14 @@ public:
 	Player(const std::string& name)
 		: m_Name(name){}
 
-	std::string GetName() { return m_Name; }
+	//Mark this overridden
+	//function with the keyword override
+	std::string GetName() override { return m_Name; }
 };
+
+void PrintName(Entity* enitity) {
+	std::cout << enitity->GetName() << std::endl;
+}
 
 void LearnVirtualFunctions() {
 	LOG("*******************************");
@@ -28,9 +54,15 @@ void LearnVirtualFunctions() {
 
 
 	Entity* e = new Entity();
-	std::cout << e->GetName() << std::endl;
+	//std::cout << e->GetName() << std::endl;
+	PrintName(e);
 	
 	Player* p = new Player("Andre");
-	std::cout << p->GetName() << std::endl;
+	//std::cout << p->GetName() << std::endl;
+	PrintName(p);
+
+	//Entity* entity = p;
+	//std::cout << entity->GetName() << std::endl;
+
 
 }
