b566f53360450a77af47c45e8f8cb0bdb00050ca
Andre Jarboe <andrejarboe@gmail.com>
2023-03-25 15:09:32 -0500

    book type notes

--- bookRepo/bookRepo/BookType.h
+++ bookRepo/bookRepo/BookType.h
@@ -3,5 +3,9 @@
 
 enum BootType { UNDECIDED, FICTION, NONFICTION };
 
-/*Parallel array pops out a string for each book type - use the book type as an index */
+/*
+* Parallel array pops out a string for each book type - use the book type as an index 
+* The enum value allows you to do cout << bookTypeStrings[UNDECIDED]; and get:
+* UNDECIDED
+*/
 static const std::string bookTypeStrings[] = { "UNDECIDED", "FICTION", "NONFICTION" }
